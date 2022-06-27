83ecef8bc6347af71c518eb5ff0ceb2160748939
James Liu <liuxjames88@gmail.com>
2022-06-26 23:48:13 -0400

    adding more endpoints

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ app.listen( PORT, () => {
   console.log(`server up on http://localhost:${PORT}`);
 })
 
+app.get( '/', (req, res) => {
+  res.send('hi');
+})
+
 
 app.get( '/version', (req, res) => {
   res.status(200).json({version: 0.1});
