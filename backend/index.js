36253c4d6d7f3b52237f821e1f61687bebad2e11
James Liu <liuxjames88@gmail.com>
2022-06-26 23:51:03 -0400

    trying to fix everything crashing on heroku

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000 | process.env.PORT;
+const PORT = process.env.PORT | 3000;
 
 app.listen( PORT, () => {
   console.log(`server up on http://localhost:${PORT}`);
