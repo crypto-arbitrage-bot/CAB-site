0e0c26d3428a64785264bab80d0805570cadba3a
james <liuxjames88@gmail.com>
2022-06-27 20:15:43 -0400

    creating templates for pages

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -2,9 +2,9 @@
     <nav id="nav">
         <a href="/"><img src="@/assets/logo.png" alt="CAB logo" id="logo" ref="Home"></a>
         <ul id="links">
-            <li ref="About"> <h1><a href="/about">About</a></h1></li>
-            <li ref="Install"><h1><a href="/install">Install</a></h1></li>
-            <li ref="Contact"><h1><a href="/contact">Contact</a></h1></li>
+            <li ref="About"> <h1><RouterLink to="/about">About</RouterLink></h1></li>
+            <li ref="Install"><h1><RouterLink to="/install">Install</RouterLink></h1></li>
+            <li ref="Contact"><h1><RouterLink to="/contact">Contact</RouterLink></h1></li>
         </ul>
     </nav>
 </template>
