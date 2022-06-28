6c88991bc7fdf1d989e9fad83df723906b2139b2
james <liuxjames88@gmail.com>
2022-06-27 20:17:20 -0400

    changing anchor tags to routerlinks

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav id="nav">
-        <a href="/"><img src="@/assets/logo.png" alt="CAB logo" id="logo" ref="Home"></a>
+        <RouterLink to="/"><img src="@/assets/logo.png" alt="CAB logo" id="logo" ref="Home"></RouterLink>
         <ul id="links">
             <li ref="About"> <h1><RouterLink to="/about">About</RouterLink></h1></li>
             <li ref="Install"><h1><RouterLink to="/install">Install</RouterLink></h1></li>
