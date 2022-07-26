8198c08084493bb393809a7a6f107d892a34be9b
James Liu <liuxjames88@gmail.com>
2022-07-26 17:07:22 -0400

    add video for install instructions

--- frontend/src/views/InstallView.vue
+++ frontend/src/views/InstallView.vue
@@ -16,11 +16,14 @@
       </a>
       </v-btn>
     </div>
-    <div class="viedo-content">
-      <img src="https://via.placeholder.com/640x360.png?text=Video+Placeholder"/>
+    <div class="video-content">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/p9nkDAKQxrw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   </div>
 </template>
+
+<script>
+</script>
 
 <style scoped>
 .install {
